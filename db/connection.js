5da964010ca9d3911a3cb987af5d0c044a638d7c
daviidbartlett <daviidbartlett@gmail.com>
2018-11-23 15:24:42 +0000

    jkhgdsf

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@
 
 const ENV = process.env.NODE_ENV || 'development';
 const config = ENV === 'production'
-  ? { client: 'pg', connection: `${process.env.DB_URL}?SSL=true` }
+  ? { client: 'pg', connection: `${process.env.DATABASE_URL}?SSL=true` }
   : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
